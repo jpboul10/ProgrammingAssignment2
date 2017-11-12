788824a4000b15c94c5f0bfdef707fead6a991e7
Jon Boulton <Jon.Boulton@CVSHealth.com>
2017-11-12 11:22:49 -0700

    Updated assignment 2, Jon Boulton

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+makeCacheMatrix <- function(x = matrix()) 
+  
+        {
+        ## Initializing x and j.
+        x <<- y
+        j <- NULL
+        ## Setting the function.
+        set <- function(y) 
+          
+                {
+                x <<- y
+                j <<- NULL
+                }
+        ##Getting the function.
+        get <- function() x
+        
+        ##Set and get inverse functions.
+        setinverse <- function(inverse) j <<- inverse
+        getinverse <- function() j
+        list(set = set, get = get,
+        setinverse = setinverse, getinverse = getinverse)
+        }
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+##Solver.
+cacheSolve <- function(x) 
+        {
+        j <- x$getinverse()
+        ##Return the matrix if it is not null.
+        if (!is.null(j)) 
+                {
+                return(j)
+                }
+        data <- x$get()
+        ##Using matlab package to get "solve()" function.
+        j <- solve(data)
+        x$setinverse(j)
+        j
+        }
